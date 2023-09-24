610d579ed8aaa85e10c35b1d7149e8a2527e0f3b
admin <test@example.com>
2023-09-24 20:59:44 +0900

    nginx設定ファイルを編集

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,47 @@
-#!/bin/bash -eux
+#!/bin/bash
 
-# 設定ファイルのコピー
+# エラーが発生したらスクリプトを中断
+set -e
 
+# 実行するコマンドを出力
+set -x
 
+# Gitのmasterブランチをpull
+echo "Pulling the latest code from the master branch..."
+git pull origin main
 
-# 実行ファイルのビルド
-cd /home/isucon/webapp/go
-go build -o isucondition
+# MySQL設定ファイルをコピー
+echo "Copying MySQL configuration..."
+sudo cp etc/my.cnf /etc/mysql/my.cnf
 
-# アプリ・ミドルウェアの再起動
+# Nginx設定ファイルをコピー
+echo "Copying Nginx configuration..."
+sudo cp etc/nginx/nginx.conf /etc/nginx/
+sudo cp etc/nginx/conf.d/default.conf /etc/nginx/conf.d/
+
+# Golangディレクトリに移動
+echo "Changing directory to golang..."
+cd golang
+
+# Goビルド
+echo "Building the Go application..."
+go build -o app 
+
+# MySQLとNginxのログを初期化
+echo "Resetting MySQL and Nginx logs..."
+sudo truncate -s 0 /var/log/mysql/slow-query.log
+sudo truncate -s 0 /var/log/nginx/access.log
+
+# isu-go, mysql, nginxを再起動
+echo "Restarting isu-go, mysql, nginx..."
+sudo systemctl restart isu-go
+sudo systemctl restart mysql
 sudo systemctl restart nginx
-sudo systemctl restart mariadb
-sudo systemctl restart isucondition.go
 
-# slow query logを有効化する
-QUERY="
- set global slow_query_log_file = '/var/log/mysql/mysql-slow.log';
- set global long_query_time = 0;
- set global slow_query_log = ON;
-"
-echo $QUERY | sudo mysql -uroot
+# MySQLのスロークエリログを有効化
+#echo "Enabling MySQL slow query log..."
+#sudo mysql -u root -e "SET GLOBAL slow_query_log = 'ON';"
+#sudo mysql -u root -e "SET GLOBAL long_query_time = 0.1;"
+#sudo mysql -u root -e "SET GLOBAL slow_query_log_file = '/var/log/mysql/slow-query.log';"
 
-# log permission
-sudo chmod 777 /var/log/nginx /var/log/nginx/*
-sudo chmod 777 /var/log/mysql /var/log/mysql/*
+echo "All tasks completed successfully."
