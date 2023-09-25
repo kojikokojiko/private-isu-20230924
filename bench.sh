11345a50c70cc46b857505875013ae94df938a4f
admin <test@example.com>
2023-09-25 16:01:58 +0900

    edit bench.sh

--- bench.sh
+++ bench.sh
@@ -8,3 +8,28 @@ sudo query-digester -duration 70
 
 # バックグラウンドのプロセスが終了するのを待つ
 wait
+
+
+# .digestファイルをwebapp下まで持ってくる
+
+echo "Copying process start!"
+SOURCE_DIR="/tmp"
+DEST_DIR="/home/isucon/private_isu/webapp/digest-log"
+
+# DEST_DIR が存在しなければ作成
+if [ ! -d "$DEST_DIR" ]; then
+    mkdir -p "$DEST_DIR"
+fi
+
+# /tmp ディレクトリ内の .digest ファイルを列挙し、それをコピーする
+for file in "$SOURCE_DIR"/*.digest; do
+    # ファイルが存在しない、または既に目的地に同名のファイルが存在する場合、スキップする
+    [ ! -e "$file" ] && continue
+    dest_file="$DEST_DIR/$(basename "$file")"
+    [ -e "$dest_file" ] && continue
+
+    # ファイルをコピーする
+    cp "$file" "$DEST_DIR/"
+done
+
+echo "Copying process completed!"
